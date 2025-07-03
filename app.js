058e5aa47780b20ee4461c0fb9309da8871df630
zubijawad <zubijawad495@gmail.com>
2025-07-03 11:12:38 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,240 @@
+// chapter 9to12
+// answer1
+city = input("Enter the city name:");
+if city.lower() == "Karachi";
+print("Welcome to city of lights")
+// answer2
+gender = input("Enter your gender (male/female:")
+ if gender.lower() =="male":
+ print("Good Morning Sir")
+ elif gender.lower() =="female":
+ print("Good Morning Ma' am")
+ else:
+ print("Good morning!")
+//  answer3
+single_color = input("Enter the color of the traffic signal (Red, Yellow, or Green):") .lower()
+if signal_color == "red";
+print("Message: Must stop")
+elief signal_color =="Yellow";
+print("Message: Ready to move")
+elief signal_color == "green";
+print("Message: Move now")
+else;
+print("invalid input. please enter red, yellow, or green.")
+// answer4
+var fuel = parseFloat(prompt("Enter theremainig fuel in your car(in litres):"));
+if (fuel <0.25) {
+    alert("please refill the fuel in your car");
+}
+// answer5
+var a = 4;
+if (++a === 5){
+    alert ("given condition for variable a is true")
+}
+var b = 82;
+if (b++ == 83):
+82 == 83
+alert ("given condition for variable b is true");
+var c = 12;
+if (c++ == 13):
+12 == 13
+alert("condition 1 is true");
+if (c == 13):
+13 == 13 is true . 
+alert("condition 2 is true");
+if (++c < 14):
+14 < 14 false
+alert("condition 3 is true");
+if (c == 14):
+14 == 14 is true
+alert("condition 4 is true");
+d . cost calculation 
+1 . variable initialization:
+materialCost = 20000 
+2 . Total cost calcilation:
+materialCost + laborCost 
+20000 + 20000 = 22000
+3 . conditional check:
+(totalCost == laborCost + materialCost) is true 
+4 . output:
+alert("The cost equals");
+e . Boolean Condotionals 
+1 . first conditional:
+if (true)
+    2 . first output:
+alert("True");
+3 . second conditional:
+if (false)
+    4 . second output:
+alert("False");
+// answer6
+if percentage >= 80:
+return "A-one", "Good"
+elief percentage >= 60:
+return "B" , "you need to improve"
+else:
+return "Fail" , "sorry"
+subject1_marks = float(input("Enter marks for subject 1:"))
+subject2_marks = float(input("Enter marks for subject 2:"))
+subject3_marks = float(input("Enter marks for subject 3:"))
+total_possible_marks = float(input("Enter total possible marks: "))
+obtained_marks = subject1_marks + subject2_marks + subject3_marks
+percentage = (obtained_marks / total_possible_marks) * 100
+grade , remarks = calculate_grade(percentage)
+print("\n--- Marks Sheet ---")
+print(f"Total Marks: {total_possible_marks}")
+print(f"Marks Obtained: {obtained_marks}")
+print(f"percentage: {percentage: .2f}%")
+print(f"Grade: {grade}")
+print(f"Remarks: {remarks}")
+// answer7
+var = (1to10)
+a . users guess is equal to secretNumber 
+b . guess + 1 is equal to secretNumber 
+// answer8
+num = int(input("Enter a number:"))
+if num % 3 == 0:
+print(f"The number {num} is divisible by 3 ")
+// answer9
+int Number;
+std::cout << " Enter an integer: ";
+std::cin >> Number;
+if (number % 2 == 0) {
+    std::cout << number << "is an even number ." << std::endl;
+} else {
+    std::cout << number << "is an odd number ." << std::endl
+}
+return 0;
+// answer10
+temperature = float(input("Enter the temperature:"))
+if temperature > 40:
+print("it is too hot outside")
+elif temperature > 30:
+print("it is too hot outside")
+elif temperature > 20:
+print("Today Weather is cool")
+elif temperature > 10:
+print("OMG! Today weather is so cool")
+else;
+print("It quied cold!")
+// answer11
+// chapter 9to12
+// answer1
+city = input("Enter the city name:");
+if city.lower() == "Karachi";
+print("Welcome to city of lights")
+// answer2
+gender = input("Enter your gender (male/female:")
+ if gender.lower() =="male":
+ print("Good Morning Sir")
+ elif gender.lower() =="female":
+ print("Good Morning Ma' am")
+ else:
+ print("Good morning!")
+//  answer3
+single_color = input("Enter the color of the traffic signal (Red, Yellow, or Green):") .lower()
+if signal_color == "red";
+print("Message: Must stop")
+elief signal_color =="Yellow";
+print("Message: Ready to move")
+elief signal_color == "green";
+print("Message: Move now")
+else;
+print("invalid input. please enter red, yellow, or green.")
+// answer4
+var fuel = parseFloat(prompt("Enter theremainig fuel in your car(in litres):"));
+if (fuel <0.25) {
+    alert("please refill the fuel in your car");
+}
+// answer5
+var a = 4;
+if (++a === 5){
+    alert ("given condition for variable a is true")
+}
+var b = 82;
+if (b++ == 83):
+82 == 83
+alert ("given condition for variable b is true");
+var c = 12;
+if (c++ == 13):
+12 == 13
+alert("condition 1 is true");
+if (c == 13):
+13 == 13 is true . 
+alert("condition 2 is true");
+if (++c < 14):
+14 < 14 false
+alert("condition 3 is true");
+if (c == 14):
+14 == 14 is true
+alert("condition 4 is true");
+d . cost calculation 
+1 . variable initialization:
+materialCost = 20000 
+2 . Total cost calcilation:
+materialCost + laborCost 
+20000 + 20000 = 22000
+3 . conditional check:
+(totalCost == laborCost + materialCost) is true 
+4 . output:
+alert("The cost equals");
+e . Boolean Condotionals 
+1 . first conditional:
+if (true)
+    2 . first output:
+alert("True");
+3 . second conditional:
+if (false)
+    4 . second output:
+alert("False");
+// answer6
+if percentage >= 80:
+return "A-one", "Good"
+elief percentage >= 60:
+return "B" , "you need to improve"
+else:
+return "Fail" , "sorry"
+subject1_marks = float(input("Enter marks for subject 1:"))
+subject2_marks = float(input("Enter marks for subject 2:"))
+subject3_marks = float(input("Enter marks for subject 3:"))
+total_possible_marks = float(input("Enter total possible marks: "))
+obtained_marks = subject1_marks + subject2_marks + subject3_marks
+percentage = (obtained_marks / total_possible_marks) * 100
+grade , remarks = calculate_grade(percentage)
+print("\n--- Marks Sheet ---")
+print(f"Total Marks: {total_possible_marks}")
+print(f"Marks Obtained: {obtained_marks}")
+print(f"percentage: {percentage: .2f}%")
+print(f"Grade: {grade}")
+print(f"Remarks: {remarks}")
+// answer7
+var = (1to10)
+a . users guess is equal to secretNumber 
+b . guess + 1 is equal to secretNumber 
+// answer8
+num = int(input("Enter a number:"))
+if num % 3 == 0:
+print(f"The number {num} is divisible by 3 ")
+// answer9
+int Number;
+std::cout << " Enter an integer: ";
+std::cin >> Number;
+if (number % 2 == 0) {
+    std::cout << number << "is an even number ." << std::endl;
+} else {
+    std::cout << number << "is an odd number ." << std::endl
+}
+return 0;
+// answer10
+temperature = float(input("Enter the temperature:"))
+if temperature > 40:
+print("it is too hot outside")
+elif temperature > 30:
+print("it is too hot outside")
+elif temperature > 20:
+print("Today Weather is cool")
+elif temperature > 10:
+print("OMG! Today weather is so cool")
+else;
+print("It quied cold!")
+// answer11
